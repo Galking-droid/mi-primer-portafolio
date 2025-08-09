a6a827b98897ac7ec7ecf7418a258b66b84439f0
Galking-droid <gama.joseph@gmail.com>
2025-08-09 14:57:17 -0500

    feat: Finalizar estilos y textos del portafolio

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
-let titulo = document.getElementById("titulo-principal");
-
-let botonTitulo = document.getElementById("boton-color");
-
-let habilidades = document.querySelector("ul");
-
-let botonHabilidades = document.getElementById("boton-habilidades");
-
-function cambiarColor() {
-  const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  titulo.style.color = color;
-}
-
-botonTitulo.addEventListener("click", cambiarColor);
-
-function alternarHabilidades() {
-  habilidades.classList.toggle("oculto");
-}
-
-botonHabilidades.addEventListener("click", alternarHabilidades);
-
 async function obtenerProyectos() {
-  //const username = "Galking-droid"; // Nombre de usuario de GitHub
-  const url = `http://localhost:3000/api/proyectos`; // URL de la API personalizada
+  const username = "Galking-droid";
+  const proyectosDestacados = [
+    "challenge-amigo-secreto-joseph-gama",
+    "juego_numero_secreto",
+    "mi-primer-portafolio",
+  ];
+  const url = `https://api.github.com/users/${username}/repos`;
   const proyectosContainer = document.getElementById("proyectos-container");
 
   try {
@@ -29,21 +13,26 @@ async function obtenerProyectos() {
     if (!response.ok) {
       throw new Error(`Error HTTP: ${response.status}`);
     }
-    const proyectos = await response.json();
+    const todosLosProyectos = await response.json();
+    const proyectosFiltrados = todosLosProyectos.filter((proyecto) =>
+      proyectosDestacados.includes(proyecto.name)
+    );
+
     proyectosContainer.innerHTML = "";
 
-    proyectos.forEach((proyecto) => {
+    // Aquí está el cambio: usa 'proyectosFiltrados'
+    proyectosFiltrados.forEach((proyecto) => {
       const proyectoDiv = document.createElement("div");
       proyectoDiv.classList.add("proyecto-item");
 
       const titulo = document.createElement("h3");
-      titulo.textContent = proyecto.nombre;
+      titulo.textContent = proyecto.name;
 
       const descripcion = document.createElement("p");
-      descripcion.textContent = proyecto.descripcion || "Sin descripción";
+      descripcion.textContent = proyecto.description || "Sin descripción";
 
       const enlace = document.createElement("a");
-      enlace.href = proyecto.url;
+      enlace.href = proyecto.html_url;
       enlace.textContent = "Ver proyecto";
       enlace.target = "_blank";
 
